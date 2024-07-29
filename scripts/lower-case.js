57d8b06a491eb7a476ea460fbbf088213f7e687f
zengxiaoluan <me@zengxiaoluan.com>
2024-07-29 19:53:53 +0800

    chore: npm run fileNameIsLowerCase

---
+++ scripts/lower-case.js
@@ -0,0 +1,23 @@
+import { execSync } from "node:child_process";
+import { kill } from "node:process";
+
+export function getChangedFiles() {
+  let files = execSync("git diff HEAD --name-only --diff-filter=d")
+    .toString()
+    .split("\n")
+    .filter((_) => _);
+
+  return files;
+}
+
+async function main() {
+  let files = await getChangedFiles();
+  for (const file of files) {
+    if (/[A-Z]/.test(file)) {
+      console.log(file);
+      kill(-1);
+    }
+  }
+}
+
+main();
