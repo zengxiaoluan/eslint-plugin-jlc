70860b6dc23450fac7017f8539d3035604058156
zengxiaoluan <me@zengxiaoluan.com>
2024-08-28 19:13:23 +0800

    add stylistic/eslint-plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import pluginJs from "@eslint/js";
 import pluginNode from "eslint-plugin-n";
 import eslintPlugin from "eslint-plugin-eslint-plugin";
+import stylistic from "@stylistic/eslint-plugin";
 
 import * as jlc from "./lib/index.js";
 
@@ -8,6 +9,7 @@ export default [
   pluginJs.configs.recommended,
   ...pluginNode.configs["flat/mixed-esm-and-cjs"],
   eslintPlugin.configs["flat/recommended"],
+  // stylistic.configs["recommended-flat"],
 
   {
     files: ["**/*.js"],
@@ -15,10 +17,14 @@ export default [
       sourceType: "commonjs",
       ecmaVersion: "latest",
     },
-    // Using the eslint-plugin-example plugin defined locally
-    plugins: { jlc },
+    // Using the eslint-plugin-jlc plugin defined locally
+    plugins: { jlc, "@stylistic": stylistic },
     rules: {
       "jlc/kebab-case": "error",
+
+      "@stylistic/indent": ["error", 2],
+      "@stylistic/quotes": ["error", "double"],
+      "@stylistic/semi": ["error", "always"],
     },
   },
 ];
